f1aef9f82a7ff191103a413ab018b42dc9f58abb
BhaveshVaru <bhavesh.varu97@gmail.com>
2020-10-02 23:36:24 +0530

    resolved cors error

--- index.server.js
+++ index.server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 //routes
 const authRoutes = require('./src/routes/auth');
@@ -36,6 +37,7 @@ app.use('/api', adminRoutes);
 app.use('/api', categoryRoutes);
 app.use('/api', productRoutes);
 app.use('/api', cartRoutes);
+app.use(cors());
 
 //listning of server
 app.listen(process.env.PORT, () => {
